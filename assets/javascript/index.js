49eec09816f4ceb006e3a575e7b53288ba84b1c2
victorviageiro <victorfelizardoviageiro@gmail.com>
2020-11-21 13:54:48 +0200

    Menu Pronto

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,5 +1,15 @@
 const menuToggle = document.querySelector('.navbar-toggler');
 const menu = document.querySelector('.navbar-nav');
-menuToggle.addEventListener('click',()=>{
+const overlayMenu = document.querySelector('.overlay');
+
+console.log(overlayMenu);
+
+menuToggle.addEventListener('click',mudarMenu);
+
+overlayMenu.addEventListener('click',mudarMenu);
+
+
+function mudarMenu(){
     menu.classList.toggle('hide-nav');
-})
+    overlayMenu.classList.toggle('show');
+}
